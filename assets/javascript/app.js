6d1f1e9f16d3b0aa15322773b333fd39fbbb7422
Monica Amitin <monzielvsu@gmail.com>
2019-10-20 20:29:24 -0700

    created vars and questionList function with one question in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,19 @@ var gotWrong = 0;
 var secondsLeft = 10;
 var timerHandle;
 
-//create 10 questions with list of multiple choice answers
+//create var for question list
+var questionList = [
+    //create question
+    {question:"What color is Marge's hair?",
+    //create list of multiple choice answers
+        possibilities: ['blue', 'pink', 'green'],
+    //create answer to question
+        answer: 0
+},
+
+
+
+]
 
 
 //start game after page finishes loading
