b4e58da7520e375528005299c4912a994a23924f
Monica Amitin <monzielvsu@gmail.com>
2019-10-20 20:21:45 -0700

    added basic html code and psuerdocode to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+//create vars
+var gotRight = 0;
+var gotWrong = 0;
+var secondsLeft = 10;
+var timerHandle;
+
+//create 10 questions with list of multiple choice answers
+
+
+//start game after page finishes loading
+//create toggle
+//call startGame function
+//call gameListeners function
+//call gameGo
+
+//create funtion startGame
+
+//create function gameListeners
+
+//create function gameGo
+
+//create function timerSeconds
+//create if or else statements within timerSeconds function
+
+//create function gameSubmit
