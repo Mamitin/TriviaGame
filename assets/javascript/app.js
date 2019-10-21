63c9e8763b5a954419f69b48371e064ea9e033b2
Amitin <MAmitin@academyart.edu>
2019-10-21 15:03:02 -0700

    updated startGame function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,70 +21,70 @@ function startGame() {
     var questionEl = '';
     var elPossibilities = [];
     //create for loop to iterate through the question & possibilities
-    for(i = 0; < questionList.length; i++) {
-        questionEl += '<div class="question" id="q'+i+'">'+questionList[i].question;
+    for (i = 0; < questionList.length; i++) {
+        questionEl += '<div class="question" id="q' + i + '">' + questionList[i].question;
         elPossibilities = questionList[i].possibilities;
-        for(j = 0; j<elPossibilities.length; j++) {
-            questionEl += '<input type="radio" class="i'+i+'" value="'+j+'">'+elPossibilities[j]+'</input>';
+        for (j = 0; j < elPossibilities.length; j++) {
+            questionEl += '<input type="radio" class="i' + i + '" value="' + j + '">' + elPossibilities[j] + '</input>';
+        }
+
+        questionEl += '</div>';
+        //console.log(questionEl);
     }
-    
-    questionEl += '</div>';
-    //console.log(questionEl);
-}
 
 
 
 
-//create var for question list
-var questionList = [
-    //create question
-    {
-        question: "What color is Marge's hair?",
-        //create list of multiple choice answers
-        possibilities: ['blue', 'pink', 'green'],
-        //answer to question
-        answer: 0
-    },
-    {
-        question: "In which town do the Simpsons reside?",
-        possibilities: ['Shelbyville', 'Springfield', 'Seinfeld'],
-        answer: 1
-    },
-    {
-        question: "How old is Bart Simpson?",
-        possibilities: ['10', '12', '11'],
-        answer: 0
-    },
-    {
-        question: "Which one of these is not a catchphrase bart uses?",
-        possibilities: ['Aye Carumba!', 'Dont have a cow, man!', 'Woohoo!'],
-        answer: 2
-    },
-    {
-        question: "What is the name of the Simpsons next door neighbor?",
-        possibilities: ['Barney Gumble', 'Ned Flanders', 'Principal Skinner'],
-        answer: 1
-    },
-    {
-        question: "Who is Mr. Burns assistant?",
-        possibilities: ['Seymour Skinner', 'Barnard Gumble', 'Waylon Smithers'],
-        answer: 2
-    },
-    {
-        question: 'Who runs the Kwik-E-Mart?',
-        possibilities: ['Apu', 'Roger', 'Mr. Burns'],
-        answer: 0
-    },
-    {
-        question: "What is the name if the school bus driver?",
-        possibilities: ['Otto', 'Martin', 'Cletus'],
-        answer: 0
-    },
-    {
-        question: "What is the name of the clown on Channel 6?",
-        possibilities: ['Gabbo', 'Krusty', 'Bonko'],
-        answer: 1
-    }]
+    //create var for question list
+    var questionList = [
+        //create question
+        {
+            question: "What color is Marge's hair?",
+            //create list of multiple choice answers
+            possibilities: ['blue', 'pink', 'green'],
+            //answer to question
+            answer: 0
+        },
+        {
+            question: "In which town do the Simpsons reside?",
+            possibilities: ['Shelbyville', 'Springfield', 'Seinfeld'],
+            answer: 1
+        },
+        {
+            question: "How old is Bart Simpson?",
+            possibilities: ['10', '12', '11'],
+            answer: 0
+        },
+        {
+            question: "Which one of these is not a catchphrase bart uses?",
+            possibilities: ['Aye Carumba!', 'Dont have a cow, man!', 'Woohoo!'],
+            answer: 2
+        },
+        {
+            question: "What is the name of the Simpsons next door neighbor?",
+            possibilities: ['Barney Gumble', 'Ned Flanders', 'Principal Skinner'],
+            answer: 1
+        },
+        {
+            question: "Who is Mr. Burns assistant?",
+            possibilities: ['Seymour Skinner', 'Barnard Gumble', 'Waylon Smithers'],
+            answer: 2
+        },
+        {
+            question: 'Who runs the Kwik-E-Mart?',
+            possibilities: ['Apu', 'Roger', 'Mr. Burns'],
+            answer: 0
+        },
+        {
+            question: "What is the name if the school bus driver?",
+            possibilities: ['Otto', 'Martin', 'Cletus'],
+            answer: 0
+        },
+        {
+            question: "What is the name of the clown on Channel 6?",
+            possibilities: ['Gabbo', 'Krusty', 'Bonko'],
+            answer: 1
+        }]
 
 
 
